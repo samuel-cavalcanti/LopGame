4bf44651d64d0ec0abd0e5a37739ffb913984764
samuel-cavalcanti <scavalcanti111@gmail.com>
2018-07-12 04:16:24 -0300

    first commit

---
+++ maps/player_script/Player.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+var motion = Vector2()
+const velPlayer = 750
+
+
+func _physics_process(delta):
+	#motion.y += gravity
+	
+	if Input.is_action_pressed('ui_right'):
+		motion.x = velPlayer
+	elif Input.is_action_pressed('ui_left'):
+		motion.x = -velPlayer
+	elif Input.is_action_pressed('ui_up'):
+		motion.y = -velPlayer
+	elif Input.is_action_pressed('ui_down'):
+		motion.y = velPlayer
+		
+	else:
+		motion.y = 0
+		motion.x = 0
+		
+	move_and_slide(motion)
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
